7365982ef44a9319cbe0de7df68495d6393a7503
Abhra Kar <abhra.kar@gmail.com>
2017-08-12 18:14:56 +0530

    Add files via upload

---
+++ IterativePostOrderTraversal.java
@@ -0,0 +1,45 @@
+package Tree;
+
+import java.util.Stack;
+
+import Tree.BinaryTree.Node;
+
+public class IterativePostOrderTraversal {
+	public static void main(String[] args) {
+
+		BinaryTree tree = new BinaryTree();
+		tree.addBSTItr(3);
+		tree.addBSTItr(6);
+		tree.addBSTItr(8);
+		tree.addBSTItr(2);
+		tree.addBSTItr(11);
+		tree.addBSTItr(13);
+		tree.addBSTItr(9);
+		tree.addBSTItr(5);
+		tree.addBSTItr(7);
+
+		postTraversal(tree.root);
+	}
+
+	static void postTraversal(Node node) {
+		Stack<Node> stack1 = new Stack<>();
+		Stack<Integer> stack2 = new Stack<>();
+
+		stack1.push(node);
+
+		while (!stack1.isEmpty()) {
+			Node temp = stack1.pop();
+			stack2.push(temp.data);
+
+			if (temp.leftChild != null)
+				stack1.push(temp.leftChild);
+
+			if (temp.rightChild != null)
+				stack1.push(temp.rightChild);
+		}
+
+		for (int i = 0; i < stack2.size(); i++)
+			System.out.print(stack2.get(i) + " ");
+	}
+
+}
