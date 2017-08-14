30e85894023d7fa5e690d92f08163fc86ba1d1b6
Abhra Kar <abhra.kar@gmail.com>
2017-08-14 10:32:50 +0530

    Add files via upload

---
+++ IsBTaBST.java
@@ -0,0 +1,29 @@
+package Tree;
+
+import Tree.BinaryTree.Node;
+
+public class IsBTaBST {
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+		tree.addBSTItr(3);
+		tree.addBSTItr(6);
+		tree.addBSTItr(8);
+		tree.addBSTItr(2);
+		tree.addBSTItr(11);
+		tree.addBSTItr(13);
+		tree.addBSTItr(9);
+
+		System.out.println(isBST(tree.root, Integer.MIN_VALUE,
+				Integer.MAX_VALUE));
+	}
+
+	public static boolean isBST(Node node, int min, int max) {
+		if (node == null)
+			return true;
+		if (node.data <= min || node.data > max)
+			return false;
+
+		return isBST(node.leftChild, min, node.data)
+				&& isBST(node.rightChild, node.data, max);
+	}
+}
